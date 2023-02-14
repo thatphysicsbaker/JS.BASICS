96e77aafeb3886e71621e5a86d65e804e8847a53
Abigail <abieniye099@gmail.com>
2023-02-14 13:00:07 +0100

    First Comment

---
+++ index.js
@@ -0,0 +1,23 @@
+function multiply (number1,number2){
+    return number1 * number2;
+}
+console.log("multiply(10, 2)");
+
+// Variable names can be constructed using the following:
+// Letters A-Z , a-z,
+// Underscore _ , 
+// Numbers 0-9
+
+// let name_2;
+// let NAME;
+
+// NAME is not equal to name is not equals to Name
+
+let firstName = 'Abigail';
+let lastName = 'Ikpeteboa';
+let stuternTrack = 'Mobile Developer';
+let skill = ' Baker';
+let Color = 'Blue';
+let yearOfCountryInd = '1960';
+
+console.log("my Name is " + firstName + ". I am a " + stuternTrack + ". I am a " + skill + ". My favourite colour is " + Color + ". My country gain independence in the year " + yearOfCountryInd +"." )

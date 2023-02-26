6b945aeee6a00ca6146d5dc020188944d612bc5e
Abigail <abieniye099@gmail.com>
2023-02-26 18:05:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,15 +3,6 @@ function multiply (number1,number2){
 }
 console.log("multiply(10, 2)");
 
-// Variable names can be constructed using the following:
-// Letters A-Z , a-z,
-// Underscore _ , 
-// Numbers 0-9
-
-// let name_2;
-// let NAME;
-
-// NAME is not equal to name is not equals to Name
 
 let firstName = 'Abigail';
 let lastName = 'Ikpeteboa';
@@ -20,4 +11,4 @@ let skill = ' Baker';
 let Color = 'Blue';
 let yearOfCountryInd = '1960';
 
-console.log("my Name is " + firstName + ". I am a " + stuternTrack + ". I am a " + skill + ". My favourite colour is " + Color + ". My country gain independence in the year " + yearOfCountryInd +"." )
+console.log("my Name is " + firstName + lastName ". I am a " + stuternTrack + ". I am a " + skill + ". My favourite colour is " + Color + ". My country gain independence in the year " + yearOfCountryInd +".")
